9c6fa8cad677c59e640b948f4a0899ae60b80f53
AnoopAparajit <AnoopVAparajit@gmail.com>
2016-10-25 07:20:47 +0000

    Fourth Trial Commit

--- pir.py
+++ pir.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.IN)         #Read output from PIR motion sensor
-GPIO.setup(3, GPIO.OUT)         #LED output pin
+GPIO.setup(3, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(11, GPIO.OUT)         #LED output pin
 while True:
        i=GPIO.input(11)
        if i==0:                 #When output from motion sensor is LOW
