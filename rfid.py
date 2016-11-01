ff5cb4c889c1f06c8b424921ce123d7f0d88cb78
AnoopAparajit <AnoopVAparajit@gmail.com>
2016-11-01 17:54:38 +0000

    Ninth trial commit

--- rfid.py
+++ rfid.py
@@ -1,23 +1,17 @@
-import RPi.GPIO as GPIO
+import time
 import serial
-import os, time
+          
+ser = serial.Serial(
+              
+    port='/dev/ttyS0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+counter=0
 
-#Enable Serial Communication
-port = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=0.01)
- 
-# Find a character in a string
-def find(str, ch):
-    for i, ltr in enumerate(str):
-        if ltr == ch: 
-           yield i
- 
-fd=''
-while True:
-    # Read the port
-    rcv = port.read(10)
-    if len(rcv) > 1:
-        fd=fd+rcv
-        ps=fd.find('\r')
-        if ps >= 0:
-            print fd[0:ps]
-            fd=''
+while 1:
+    x=ser.readline()
+    print x
